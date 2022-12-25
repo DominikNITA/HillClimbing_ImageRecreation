d414e331a6895dd62ac1ad3dcc0cf68da3a93f23
DominikNITA <36695471+DominikNITA@users.noreply.github.com>
2022-12-25 20:58:42 +0100

    Added Row and Column components. Started working on Parameters component

--- Logic/Models/AlgorithmParameters.cs
+++ Logic/Models/AlgorithmParameters.cs
@@ -4,18 +4,26 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.ComponentModel.DataAnnotations;
 namespace Logic.Models
 {
     public class AlgorithmParameters
     {
+        [Required]
         public int MaxIterations { get; set; }
         public Color BackgroundColor { get; set; }
+        [Required]
         public IEnumerable<Shape>? Shapes { get; set; }
+        [Required]
         public int MaxShapeSize { get; set; }
+        [Required]
         public int MinShapeSize { get; set; }
+        public int[] ShapeSizeLimits { get; set; }
+        [Required]
         public bool UseColorDict { get; set; }
+        [Required]
         public bool AllowRotation { get; set; }
+        [Required]
         public bool AllowAlpha { get; set; }
     }
 
